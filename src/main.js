02cbea3ba581076d4c3daaf83af6d413c28f91d5
molly <xiaoxiaojin@201314.com>
2020-02-07 12:15:09 +0800

    初始化-整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由
+import router from './router/index'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂载到vue实例上
+  router
 }).$mount('#app')
