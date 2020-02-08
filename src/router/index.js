586bee31ff86c51a627ac5f2a96a86a2f375f701
molly <xiaoxiaojin@201314.com>
2020-02-08 10:16:54 +0800

    移除多余代码

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,6 @@ import login from '../views/login/login.vue'
 // 注册路由
 Vue.use(VueRouter);
 
-// 创建路由规则
-// const routes = [{
-//   path:'/login',
-//   component:login
-// }];
-
 // 创建路由对象
 const router = new VueRouter({
   //路由规则
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,68 +1,77 @@
 <template>
-<!-- 整体容器 -->
+  <!-- 整体容器 -->
   <div class="login-container">
-      <!-- 左侧盒子 -->
-      <div class="left-box">
-          <!-- 顶部的标题 -->
-          <div class="title-box">
-              <img src="../../assets/login-logo.png" alt="">
-              <span class="title">黑马面面</span>
-              <span class="line"></span>
-              <span class="sub-title">用户登录</span>
-          </div>
+    <!-- 左侧盒子 -->
+    <div class="left-box">
+      <!-- 顶部的标题 -->
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
       </div>
-      <!-- 右边图片 -->
-      <img src="../../assets/login_banner_ele.png" alt="" class="logo">
+
+      <!-- 输入框 -->
+      
+    </div>
+    <!-- 右边图片 -->
+    <img src="../../assets/login_banner_ele.png" alt class="logo" />
   </div>
 </template>
 
 <script>
 export default {
-   name:"login"
-}
+  name: "login",
+ 
+};
 </script>
 
 <style lang="less">
-  .login-container{
-      height: 100%;
-      background:linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
-      /* 开启弹性布局 */
+.login-container {
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  /* 开启弹性布局 */
+  display: flex;
+  /* 上下居中 */
+  align-items: center;
+  /* 左右均分 */
+  justify-content: space-around;
+
+  .left-box {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+
+    .title-box {
       display: flex;
-      /* 上下居中 */
       align-items: center;
-      /* 左右均分 */
-      justify-content: space-around;
-
-      .left-box {
-        width: 478px;
-        height: 550px;
-        background:rgba(245,245,245,1);
-
-        .title-box {
-            display: flex;
-            align-items: center;
-            margin-top: 44px;
-            margin-left: 48px;
-            .logo{
-                width: 22px;
-                height: 17px;
-            }
-            .title{
-                font-size: 24px;
-                margin-left: 16px;
-                margin-right: 14px;
-
-            }
-            .line{
-                width: 1px;
-                height: 28px;
-                background-color: #C7C7C7;
-            }
-            .sub-title{
-                font-size: 21px;
-                margin-left: 12px;
-            }
-        }
+      margin-top: 44px;
+      margin-left: 48px;
+      .logo {
+        width: 22px;
+        height: 17px;
       }
-   }
+      .title {
+        font-size: 24px;
+        margin-left: 16px;
+        margin-right: 14px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background-color: #c7c7c7;
+      }
+      .sub-title {
+        font-size: 21px;
+        margin-left: 12px;
+      }
+    }
+
+   
+  }
+}
 </style>
