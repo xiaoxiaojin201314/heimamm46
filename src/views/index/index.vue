16e7c0491cf148e04b37e8a50348b958daa381d0
molly <xiaoxiaojin@201314.com>
2020-02-12 14:31:26 +0800

    首页模块-开启导航功能

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,42 +2,100 @@
   <el-container class="my-container">
     <el-header class="my-header">
       <div class="left">
-        <i class="el-icon-s-fold"></i>
+        <!-- 左侧图标 -->
+        <i @click="isCollapse = !isCollapse" class="el-icon-s-fold"></i>
         <img src="../../assets/index_logo.png" alt="" />
         <span>黑马面面</span>
       </div>
       <div class="right">
         <img :src="userIcon" alt="" />
-        <span class="name">{{username}},您好</span>
-        <el-button type="primary">退出</el-button>
+        <span class="name">{{ username }},您好</span>
+        <el-button type="primary" @click="logout">退出</el-button>
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="my-aside">Aside</el-aside>
-      <el-main class="my-main">Main</el-main>
+      <!-- 侧边栏 -->
+      <el-aside width="auto" class="my-aside">
+        <el-menu router :collapse="isCollapse" :default-active="$route.path" class="el-menu-vertical-demo">
+          <el-menu-item index="/index/chart">
+            <!-- 图标 -->
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <el-menu-item index="/index/user">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/question">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/enterprise">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/subject">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu></el-aside
+      >
+      <el-main class="my-main">
+        <!-- 路由出口 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
 // 导入接口
-import { info } from '@/api/index.js';
+import { info, logout } from '@/api/index.js';
+// 导入 token函数
+import { removeToken } from '@/utils/token.js';
 export default {
   name: 'index',
-  data(){
+  data() {
     return {
       // 用户名
-      username:"",
+      username: '',
       // 用户头像
-      userIcon:""
-    }
+      userIcon: '',
+      // 是否折叠
+      isCollapse: false
+    };
   },
   created() {
-    info().then(res=>{
+    info().then(res => {
       this.username = res.data.data.username;
       // 服务器返回的头像地址不完整，需要进行拼接
-      this.userIcon =  process.env.VUE_APP_URL+"/"+res.data.data.avatar
-    })
+      this.userIcon = process.env.VUE_APP_URL + '/' + res.data.data.avatar;
+    });
+  },
+  methods: {
+    logout() {
+      this.$confirm('你确定要离开我们网站', '友情提示', {
+        confirmButtonText: '狠心离开',
+        cancelButtonText: '继续看看',
+        // type: 'error'
+        type: 'success'
+      })
+        .then(() => {
+          // 点击确定
+          logout().then(res => {
+            // window.console.log(res)
+            if (res.data.code === 200) {
+              // 移除token
+              removeToken();
+              // 去登录页
+              this.$router.push('/login');
+            }
+          });
+        })
+        .catch(() => {
+          // 点击取消
+        });
+    }
   }
 };
 </script>
@@ -78,10 +136,15 @@ export default {
     }
   }
   .my-aside {
-    background: yellowgreen;
+    // background: yellowgreen;
   }
   .my-main {
     background: #0094ff;
+  }
+  /* c3中的transition（过渡），需要有开始和结束的值 */
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
   }
 }
 </style>
---
+++ src/views/index/subject/subject.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='subject-container'>学科列表</div>
+</template>
+
+<script>
+export default {
+    name:"subject"
+}
+</script>
+
+<style>
+
+</style>
