d33598e5ce2c27b4442a3ef77e8b52d1be8fd182
molly <xiaoxiaojin@201314.com>
2020-02-14 12:47:15 +0800

    学科模块 - 顶部布局

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -1,13 +1,63 @@
 <template>
-  <div class='subject-container'>学科列表</div>
+  <div class="subject-container">
+    <!-- 顶部的盒子 -->
+    <el-card class="top-card">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="学科编号">
+          <el-input class="short" v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="学科名称">
+          <el-input class="normal" v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+
+        <el-form-item label="创建者">
+          <el-input class="short" v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+
+        <el-form-item label="状态">
+          <el-select class="normal" v-model="formInline.region" placeholder="活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" >查询</el-button>
+          <el-button >清除</el-button>
+          <el-button icon="el-icon-plus" type="primary" >新增学科</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"subject"
-}
+  name: "subject",
+  data() {
+    return {
+      formInline: {
+        user: "",
+        region: ""
+      }
+    };
+  }
+};
 </script>
 
-<style>
-
+<style lang='less'>
+//最外层容器
+.subject-container{
+  //顶部的卡片
+  .top-card{
+    //短的输入框
+    .short{
+      width: 100px;
+    }
+    //长的输入框
+    .normal{
+      width: 150px;
+    }
+  }
+}
 </style>
