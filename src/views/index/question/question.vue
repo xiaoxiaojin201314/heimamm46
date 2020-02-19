a15cd7e6a4a07b37aa38edfe258355646c31cc89
molly <xiaoxiaojin@201314.com>
2020-02-19 12:26:08 +0800

    题库模块 - 日期选择器整合

--- src/views/index/question/question.vue
+++ src/views/index/question/question.vue
@@ -26,27 +26,27 @@
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
         </el-form-item>
-        <br>
+        <br />
         <el-form-item label="难度">
           <el-select v-model="formInline.region" placeholder="请选择难度">
             <el-option label="区域一" value="shanghai"></el-option>
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
         </el-form-item>
-         <el-form-item label="作者">
-            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
-          </el-form-item>
-          <el-form-item label="状态">
+        <el-form-item label="作者">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
           <el-select v-model="formInline.region" placeholder="请选择状态">
             <el-option label="区域一" value="shanghai"></el-option>
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="日期">
-          <el-input v-model="formInline.user" placeholder="选择日期"></el-input>
+          <el-date-picker v-model="formInline.value1" type="date" placeholder="选择日期"></el-date-picker>
         </el-form-item>
         <el-form-item class="title-item" label="标题">
-          <el-input v-model="formInline.user" placeholder=""></el-input>
+          <el-input v-model="formInline.user" placeholder></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary">搜索</el-button>
@@ -73,14 +73,14 @@ export default {
 </script>
 
 <style lang="less">
-  .question-container {
+.question-container {
   //  所有的盒子 除了最后一个 都设置宽度
   // .el-form--inline .el-form-item__content {
   .el-form--inline .el-form-item:not(:last-child) .el-form-item__content {
     width: 150px;
   }
   // 标题的盒子
-   .el-form--inline .el-form-item.title-item .el-form-item__content{
+  .el-form--inline .el-form-item.title-item .el-form-item__content {
     width: 398px;
   }
   .top-card .el-card__body {
@@ -89,6 +89,10 @@ export default {
   .el-form-item__label {
     padding-left: 30px;
     padding-right: 30px;
+  }
+  //选择日期的宽度
+  .el-date-editor.el-input{
+    width: 150px;
   }
 }
 </style>
